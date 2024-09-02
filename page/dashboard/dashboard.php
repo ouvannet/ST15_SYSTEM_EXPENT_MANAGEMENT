fc6d1125ae2541e5558b5ea5cede9ae546b6f228
ryanpetter120 <ryanpetter120@gmail.com>
2024-09-02 08:47:49 +0700

    test

--- page/dashboard/dashboard.php
+++ page/dashboard/dashboard.php
@@ -16,7 +16,7 @@
                                             </div>
                                             <div class="col-md-8">
                                                 <h6 class="text-muted font-semibold">Profile Views</h6>
-                                                <h6 class="font-extrabold mb-0">112.000</h6>
+                                                <h6 class="font-extrabold mb-0">1212.000</h6>
                                             </div>
                                         </div>
                                     </div>
